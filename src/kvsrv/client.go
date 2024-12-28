d7ae56c54b5b49ea3a5c5cd8f4d2f8fe14a3f53f
Alan Wang <wzh19921016@gmail.com>
2024-12-27 18:28:15 -0800

    lab 2 all pass

--- src/kvsrv/client.go
+++ src/kvsrv/client.go
@@ -10,6 +10,7 @@ import (
 type Clerk struct {
 	server *labrpc.ClientEnd
 	// You will have to modify this struct.
+	id int64
 }
 
 func nrand() int64 {
@@ -23,6 +24,7 @@ func MakeClerk(server *labrpc.ClientEnd) *Clerk {
 	ck := new(Clerk)
 	ck.server = server
 	// You'll have to add code here.
+	ck.id = nrand()
 	return ck
 }
 
@@ -39,10 +41,18 @@ func MakeClerk(server *labrpc.ClientEnd) *Clerk {
 func (ck *Clerk) Get(key string) string {
 
 	// You will have to modify this function.
-	args := GetArgs{}
-	args.Key = key
+	args := GetArgs{
+		Key:       key,
+		RequestId: nrand(),
+		ClientId:  ck.id,
+	}
 	reply := GetReply{}
-	ck.server.Call("KVServer.Get", &args, &reply)
+	for {
+		success := ck.server.Call("KVServer.Get", &args, &reply)
+		if success {
+			break
+		}
+	}
 	return reply.Value
 }
 
@@ -57,11 +67,18 @@ func (ck *Clerk) Get(key string) string {
 func (ck *Clerk) PutAppend(key string, value string, op string) string {
 	// You will have to modify this function.
 	args := PutAppendArgs{
-		Key:   key,
-		Value: value,
+		Key:       key,
+		Value:     value,
+		RequestId: nrand(),
+		ClientId:  ck.id,
 	}
 	reply := PutAppendReply{}
-	ck.server.Call("KVServer."+op, &args, &reply)
+	for {
+		success := ck.server.Call("KVServer."+op, &args, &reply)
+		if success {
+			break
+		}
+	}
 	return reply.Value
 }
 
--- src/kvsrv/server.go
+++ src/kvsrv/server.go
@@ -15,38 +15,62 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 }
 
 type KVServer struct {
-	mu   sync.Mutex
-	data map[string]string
+	mu            sync.Mutex
+	data          map[string]string
+	cacheRequest  map[int64]int64
+	cacheResponse map[int64]string
+}
+
+func (kv *KVServer) checkCache(clientId int64, requestId int64, reply *string) bool {
+	prevRequest, newClient := kv.cacheRequest[clientId]
+	if !newClient || prevRequest != requestId {
+		return false
+	}
+	*reply = kv.cacheResponse[clientId]
+	return true
 }
 
 func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
 	// Your code here.
 	kv.mu.Lock()
 	res, exist := kv.data[args.Key]
-	kv.mu.Unlock()
 	if !exist {
 		res = ""
 	}
 	reply.Value = res
+	kv.mu.Unlock()
 }
 
 func (kv *KVServer) Put(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
 	kv.mu.Lock()
+	if kv.checkCache(args.ClientId, args.RequestId, &reply.Value) {
+		kv.mu.Unlock()
+		return
+	}
+	kv.cacheRequest[args.ClientId] = args.RequestId
 	kv.data[args.Key] = args.Value
+	reply.Value = ""
+	kv.cacheResponse[args.ClientId] = ""
 	kv.mu.Unlock()
 }
 
 func (kv *KVServer) Append(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
 	kv.mu.Lock()
+	if kv.checkCache(args.ClientId, args.RequestId, &reply.Value) {
+		kv.mu.Unlock()
+		return
+	}
+	kv.cacheRequest[args.ClientId] = args.RequestId
 	res, exist := kv.data[args.Key]
 	if !exist {
 		res = ""
 	}
+	kv.cacheResponse[args.ClientId] = res
+	reply.Value = res
 	kv.data[args.Key] = res + args.Value
 	kv.mu.Unlock()
-	reply.Value = res
 }
 
 func StartKVServer() *KVServer {
@@ -54,6 +78,8 @@ func StartKVServer() *KVServer {
 
 	// You may need initialization code here.
 	kv.data = make(map[string]string)
+	kv.cacheResponse = make(map[int64]string)
+	kv.cacheRequest = make(map[int64]int64)
 
 	return kv
 }
