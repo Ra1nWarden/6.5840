078f3a9cb4237335835574115525afe1ee8eaf04
Alan Wang <wzh19921016@gmail.com>
2025-06-17 14:50:40 -0700

    lab 5 start

--- src/shardctrler/server.go
+++ src/shardctrler/server.go
@@ -1,11 +1,23 @@
 package shardctrler
 
+import (
+	"log"
+	"sort"
+	"sync"
 
-import "6.5840/raft"
-import "6.5840/labrpc"
-import "sync"
-import "6.5840/labgob"
+	"6.5840/labgob"
+	"6.5840/labrpc"
+	"6.5840/raft"
+)
 
+const Debug = false
+
+func DPrintf(format string, a ...interface{}) (n int, err error) {
+	if Debug {
+		log.Printf(format, a...)
+	}
+	return
+}
 
 type ShardCtrler struct {
 	mu      sync.Mutex
@@ -14,18 +26,110 @@ type ShardCtrler struct {
 	applyCh chan raft.ApplyMsg
 
 	// Your data here.
+	cond         *sync.Cond
+	prevRequest  map[int64]int64
+	prevResponse map[int64]Config
+	latestIndex  int
+	term         int
+	isLeader     bool
 
 	configs []Config // indexed by config num
 }
 
-
 type Op struct {
 	// Your data here.
+	Operation string
+	Servers   map[int][]string
+	Num       int
+	GID       int
+	Shard     int
+	RequestId int64
+	ClientId  int64
 }
 
+// This helper function takes a groups mapping and current shard allocation,
+// and returns a new shard allocation that is balanced.
+func rebalance(shards [NShards]int, groups map[int][]string) [NShards]int {
+	allocation := make(map[int]int)
+	gids := make([]int, 0, len(groups))
+	for gid := range groups {
+		allocation[gid] = 0
+		gids = append(gids, gid)
+	}
+	sort.Ints(gids)
+
+	result := [NShards]int{}
+
+	baseAllocation := len(shards) / len(groups)
+	remainder := len(shards) % len(groups)
+
+	queue := []int{}
+
+	for shard, gid := range shards {
+		_, ok := allocation[gid]
+		if !ok {
+			result[shard] = 0
+			queue = append(queue, shard)
+			continue
+		}
+
+		if allocation[gid] < baseAllocation {
+			allocation[gid]++
+			result[shard] = gid
+		} else if allocation[gid] == baseAllocation && remainder > 0 {
+			allocation[gid]++
+			result[shard] = gid
+			remainder--
+		} else {
+			result[shard] = 0
+			queue = append(queue, shard)
+		}
+	}
+
+	for _, gid := range gids {
+		target := baseAllocation
+		if remainder > 0 {
+			target++
+			remainder--
+		}
+		count := allocation[gid]
+		for count < target {
+			shard := queue[0]
+			queue = queue[1:]
+			result[shard] = gid
+			count++
+		}
+	}
+
+	return result
+
+}
 
 func (sc *ShardCtrler) Join(args *JoinArgs, reply *JoinReply) {
 	// Your code here.
+	sc.mu.Lock()
+	defer sc.mu.Unlock()
+
+	newConfig := Config{
+		Num:    len(sc.configs),
+		Shards: [NShards]int{},
+		Groups: make(map[int][]string),
+	}
+
+	lastConfig := sc.configs[len(sc.configs)-1]
+
+	for gid, servers := range lastConfig.Groups {
+		newConfig.Groups[gid] = servers
+	}
+
+	for gid, servers := range args.Servers {
+		newConfig.Groups[gid] = servers
+	}
+
+	newConfig.Shards = rebalance(lastConfig.Shards, newConfig.Groups)
+
+	sc.configs = append(sc.configs, newConfig)
+
 }
 
 func (sc *ShardCtrler) Leave(args *LeaveArgs, reply *LeaveReply) {
@@ -39,7 +143,6 @@ func (sc *ShardCtrler) Move(args *MoveArgs, reply *MoveReply) {
 func (sc *ShardCtrler) Query(args *QueryArgs, reply *QueryReply) {
 	// Your code here.
 }
-
 
 // the tester calls Kill() when a ShardCtrler instance won't
 // be needed again. you are not required to do anything
